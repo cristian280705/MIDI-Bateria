ee9bd1109d9d5c1279c3b4195d685f167ccf46e8
Cristian Silveira <110128134+cristian280705@users.noreply.github.com>
2022-12-29 20:08:26 -0300

    Arquivos do MIDI-Bateria
    Fazendo upload de todos os arquivos do projeto MIDI-Bateria

---
+++ MIDI-Bateria/main.js
@@ -0,0 +1,36 @@
+let listaDeTeclas = document.querySelectorAll('.tecla');
+
+function tocaSom (idsom) {
+
+    document.querySelector(idsom).play();
+}
+
+for(let i = 0; i < listaDeTeclas.length; i++) {
+
+    const teclas = listaDeTeclas[i];
+    const instrumento = teclas.classList[1];
+
+    const idSom = `#som_${instrumento}`;
+
+    teclas.onclick = function (evento) {
+
+            tocaSom(idSom);
+        }
+
+    teclas.onkeydown = function (evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+
+            teclas.classList.add('ativa');
+        }
+
+    }
+
+    teclas.onkeyup = function () {
+
+        teclas.classList.remove('ativa');
+    }
+
+   
+
+}
