30ff3544f86e12558191863690c117737db86b13
Cristian Silveira <110128134+cristian280705@users.noreply.github.com>
2023-01-03 23:21:09 -0300

    adicionando comentarios explicativos para o codigo main.js

--- MIDI-Bateria/main.js
+++ MIDI-Bateria/main.js
@@ -1,16 +1,18 @@
-let listaDeTeclas = document.querySelectorAll('.tecla');
+let listaDeTeclas = document.querySelectorAll('.tecla');//Pega todas as minhas teclas
 
-function tocaSom (idsom) {
+function tocaSom (idsom) {//Me da um "idsom" de acordo com a tecla clicada, assim tocando o som certo.
 
-    document.querySelector(idsom).play();
+    document.querySelector(idsom).play();//toca o som.
 }
 
-for(let i = 0; i < listaDeTeclas.length; i++) {
+for(let i = 0; i < listaDeTeclas.length; i++) {//percorre a minha array de teclas
 
     const teclas = listaDeTeclas[i];
+    
+    //Pega a primeira classe das teclas, que a parte que falta para o idsom estar correto.
     const instrumento = teclas.classList[1];
 
-    const idSom = `#som_${instrumento}`;
+    const idSom = `#som_${instrumento}`;//Monta o idsom formatado.
 
     teclas.onclick = function (evento) {
 
@@ -18,7 +20,7 @@ for(let i = 0; i < listaDeTeclas.length; i++) {
         }
 
     teclas.onkeydown = function (evento) {
-
+        //adiciona a funcionalidade de click na tecla da bateria para essas duas teclas.
         if(evento.code === 'Space' || evento.code === 'Enter') {
 
             teclas.classList.add('ativa');
@@ -30,7 +32,4 @@ for(let i = 0; i < listaDeTeclas.length; i++) {
 
         teclas.classList.remove('ativa');
     }
-
-   
-
 }
